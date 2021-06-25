2cb0be6daedf530c58400592422ada3672b84787
Kunal Sool <kunalsool4@gmail.com>
2021-06-25 12:20:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace PracticeConsoleApp
+{
+    class Program
+    {
+        string url = "https://jsonplaceholder.typicode.com/todos";
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetJsonData();
+        }
+        private async Task GetJsonData()
+        {
+            string responese = await client.GetStringAsync(url);
+            Console.WriteLine(responese);
+            Console.ReadLine();
+        }
+    }
+}
